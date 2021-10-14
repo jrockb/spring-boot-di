79e6eb3685fb0ecae14ae0800d7735327ea13d33
jrockb <jrockb@gmail.com>
2021-10-14 14:52:39 -0500

    creacion de proyecto y creacion de controlador index

---
+++ src/main/java/co/com/jcd/springboot/di/app/controller/IndexController.java
@@ -0,0 +1,15 @@
+package co.com.jcd.springboot.di.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping({"/","","/index"})
+	public String index(Model model) {
+		return "index";		
+	}
+
+}
