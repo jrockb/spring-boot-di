f5f077157be5f4bd1c90ea564ff9fbe75ee17939
jrockb <jrockb@gmail.com>
2021-10-15 21:44:21 -0500

    Ejemplo de cargue de una clase servicio desde el controlador sin inyección de dependencias

--- src/main/java/co/com/jcd/springboot/di/app/controller/IndexController.java
+++ src/main/java/co/com/jcd/springboot/di/app/controller/IndexController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import co.com.jcd.springboot.di.app.models.services.MiServicio;
+
 @Controller
 public class IndexController {
 	
+	private MiServicio servicio = new MiServicio(); // sin usar DI, muy acoplado
+	
 	@GetMapping({"/","","/index"})
 	public String index(Model model) {
+		model.addAttribute("objeto", servicio.operacion());
 		return "index";		
 	}
 
---
+++ src/main/java/co/com/jcd/springboot/di/app/models/services/MiServicio.java
@@ -0,0 +1,10 @@
+package co.com.jcd.springboot.di.app.models.services;
+
+
+public class MiServicio {
+	
+	public String operacion() {
+		return "ejecutando algun proceso importante...";
+	}
+
+}
